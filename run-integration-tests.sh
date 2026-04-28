5edc69490b13d48a7de623fe0321de799c77601c
Chris Keith <chris.keith@gmail.com>
2026-04-27 17:02:03 -0700

    First cut at separate script. Test for server running doesn't work yet

---
+++ run-integration-tests.sh
@@ -0,0 +1,74 @@
+#! /bin/bash
+
+# DANGER !
+# This script deletes your current local integration-tests repo,
+# in an attempt to start with a clean slate.
+
+set -x
+
+# ------------------------------------------------------------
+# Build and run integration tests.
+
+cd ehrbase
+if [ $? -ne 0 ] ; then
+    echo "No ehrbase directory found. Please run this script from the parent directory of your local repo(s)."
+    exit -670
+fi	
+docker rm -f ehrdb
+	# Ignore error if container doesn't exist.
+docker run --name ehrdb --network ehrbase-net -e POSTGRES_PASSWORD=postgres -d -p 5432:5432 ehrbase/ehrbase-v2-postgres:16.2
+if [ $? -ne 0 ] ; then
+	echo "Failed postgres docker run."
+	exit -669
+fi
+
+# TODO: Check if ehrbase server is running. If so, kill it before starting a new one.
+# ck@2018-ck-nuc:~/Documents/github$ ps -aux | grep java
+# ck         22715  2.7  1.7 12279920 559548 pts/0 Sl   16:13   1:04 java -jar application/target/ehrbase.jar
+
+java -jar application/target/ehrbase.jar 2>&1 | tee ehrbase.log &
+counter=1
+while [ $counter -le 60 ]
+do
+    echo "Count: $counter"
+	started=`grep -c "Started EhrBase in" ehrbase.log`
+	if [ -n "$started" ] ; then
+		echo "EhrBase started."
+		break
+	fi
+    ((counter++))
+    sleep 5
+done
+if [ $counter -gt 60 ] ; then
+    echo "EhrBase failed to start within 5 minutes."
+    exit -671
+fi
+
+cd ..
+if [ -d "integration-tests" ] ; then
+	rm -rf integration-tests
+	if [ $? -ne 0 ] ; then
+		echo "Failed to delete local integration-tests repo."
+		exit -670
+	fi	
+fi
+git clone https://github.com/ehrbase/integration-tests.git
+if [ $? -ne 0 ] ; then
+	echo "Failed to clone integration-tests repo."
+	exit -671
+fi
+
+cd integration-tests/tests
+sed -i 's/RESTinstance == 1.8.0/RESTinstance/g' requirements.txt
+if [ $? -ne 0 ] ; then
+	echo "Failed to sed -i 's/RESTinstance == 1.8.0/RESTinstance/g' requirements.txt"
+	exit -672
+fi
+
+pip install -r ./requirements.txt
+if [ $? -ne 0 ] ; then
+	echo "Failed to pip install -r requirements.txt"
+	exit -673
+fi
+chmod a+x ./run_local_tests.sh
+./run_local_tests.sh
